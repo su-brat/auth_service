5f26e560e68d2d18736b13b9724f2296e6469c34
Subrat Kumar Pattanaik <37294184+su-brat@users.noreply.github.com>
2025-03-12 20:08:07 +0530

    feat(buggy): added swagger ui dependency

--- src/main/java/com/status_app/auth_service/AuthServiceApplication.java
+++ src/main/java/com/status_app/auth_service/AuthServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AuthGatewayApplication {
+public class AuthServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AuthGatewayApplication.class, args);
+		SpringApplication.run(AuthServiceApplication.class, args);
 	}
 
 }
