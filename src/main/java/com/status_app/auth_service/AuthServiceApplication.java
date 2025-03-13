faa8a07fe75e52d5e2d785a920f1ae74df2b035c
Subrat Kumar Pattanaik <37294184+su-brat@users.noreply.github.com>
2025-03-13 18:35:03 +0530

    refactor: moving properties to yaml

--- src/main/java/com/status_app/auth_service/AuthServiceApplication.java
+++ src/main/java/com/status_app/auth_service/AuthServiceApplication.java
@@ -1,13 +1,18 @@
 package com.status_app.auth_service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
+@Slf4j
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AuthServiceApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(AuthServiceApplication.class, args);
+		String[] activeProfiles = context.getEnvironment().getActiveProfiles();
+		log.info("environment: {}", activeProfiles[0]);
 	}
 
 }
